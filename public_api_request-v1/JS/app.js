f4a0897bff88a8627dfebf0e70b76316aa50b08a
Akhaled19 <akhaled@clarku.edu>
2020-01-01 22:04:58 -0500

    Added the skeleton

--- public_api_request-v1/JS/app.js
+++ public_api_request-v1/JS/app.js
@@ -1,1 +1,45 @@
-document.alert('testing');
+alert('testing');
+
+                                 //GLOBAL VARIABLES// 
+
+
+                                 //API Usage//
+//pull 12 random users from the API
+
+//new random employee information displays each time the page loads (?)
+
+
+                               //User Directory//     
+
+//12 random user API Info:
+    //First and Last name 
+    //Email 
+    //City or location
+
+//Employees can be filtered by name 
+    //dynamically add a search feature 
+
+                             //Modal Window//
+
+//Click event on user = open detail modal window 
+
+    //add functionality to switch back and forth between employees 
+        //beginning and end of the list !== error in the console 
+
+    //Modal window displays the following:
+        // Employee image 
+        // Name 
+        //Email 
+        //City or location 
+        //Cell Number 
+        //Detailed Address(street name & number, state or country, and post code)
+        // 'X' to close the modal window    
+
+                            //Structure, style, CSS
+
+//The elements of the directory and modal window are in place & match the mockups 
+//Add styling:
+    //color
+    //background color 
+    //font
+    //box or text shadows                             
