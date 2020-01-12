cb8b81bca94c3b4d8ff0b6dc21e044d937164ca0
Akhaled19 <akhaled@clarku.edu>
2020-01-12 17:34:28 -0500

    trying to add the users info to the html

--- public_api_request-v1/JS/app.js
+++ public_api_request-v1/JS/app.js
@@ -5,7 +5,7 @@ const galleryDiv = document.getElementsByClassName('gallery');
 
 //body
 const bodyElement = document.getElementsByTagName('body');
-const modalContainer = document.createElement('div');
+
 
                                 //API Usage//
 //pull 12 random users from the API
@@ -13,15 +13,36 @@ const modalContainer = document.createElement('div');
 
     fetch('https://randomuser.me/api/?results=12&inc=name,email,location,cell,picture')
         ,then(response => response.json() )
-        .then( data => console.log(generateHTML(data) ))
-        .catch( err => console.error(err))
+        .then( data => console.log(generateHTML(data)) )
+        .catch( err => console.error(err) )
+    
+    fetch('https://randomuser.me/api/?results=12&inc=dob')
+        .then( dobResponse => dobResponse.json() )
+        .then( dob => console.log(generateHTML(dob)) )
+        .catch( err => console.error(err) )
+         
+        
+                               //helper functions
+    //deserializing JSON dob string as JS date object 
+    //00/00/0000
+    const dateFormat = /^\d{2}-\d{2}-\d{4}/;  
+    
+    function reviver(value) {
+        if(typeof value === 'string' && dateFormat.test(value)) {
+            return new Date(value)
+        }
+        return value;
+    }
 
 
                                //User Directory//     
 
 //12 random user API Info:
-function generateHTML(data) {
+function generateImg(data) {
     const userImg = data.map(user => 
+    //First and Last name 
+    //Email 
+    //City or location
         `
         <div class="card">
             <div class="card-img-container">
@@ -34,15 +55,36 @@ function generateHTML(data) {
             </div>
          </div>
         `
-        
-
-
     )
+    galleryDiv.appendChild(userImg);
 
 }
-    //First and Last name 
-    //Email 
-    //City or location
+function generateModal(data) {
+    const userInfo = data.map(user => 
+        `
+        <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src=${user.picture[medium]} alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${user.name}</h3>
+                        <p class="modal-text">${user.email}</p>
+                        <p class="modal-text cap">${user.location[city]}</p>
+                        <hr>
+                        <p class="modal-text">${user.cell}</p>
+                        <p class="modal-text">${user.location[street]}, ${user.location[city]}, ${user.location[state]}, ${user.location[postcode]}</p>
+                        <p class="modal-text">${user.dob}Birthday: 10/21/2015</p>
+                    </div>
+                </div>
+                <div class="modal-btn-container">
+                <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                <button type="button" id="modal-next" class="modal-next btn">Next</button>
+            </div>
+        </div>
+        `
+    )
+  
+}    
 
 //Employees can be filtered by name 
     //dynamically add a search feature 
